8ad99869d8e007bc08dff838ed6503168a231c5c
Dmitriy Ivanov <dmitriy.se@gmail.com>
2019-03-02 19:01:53 +1300

    First dotnet project added with the default stylecop settings.

---
+++ src/ClrSeed/ClrCoderDefaults.cs
@@ -0,0 +1,12 @@
+// Copyright (c) ClrCoder community. All Rights Reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace ClrSeed
+{
+    /// <summary>
+    /// Global project constants and variables.
+    /// </summary>
+    public static class ClrCoderDefaults
+    {
+    }
+}
