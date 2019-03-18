c2497626a2d1b2a76de19f606a4865116245819c
Dmitriy Ivanov <dmitriy.se@gmail.com>
2019-03-18 21:53:12 +1300

    Commit messages validation rule has been implemented for git.

--- build/rules/vcs/no-relative-issue-refs.ps1
+++ build/rules/vcs/no-relative-issue-refs.ps1
@@ -74,22 +74,97 @@ function Select-HgCommitMessages {
         $curChangeset
     }
 }
-try {
 
-    if (Test-Path "$repoRoot/.hg" -Type Container) {
-        $changeSets = Invoke-HgCommand -Arguments "log", "--template", "\0{rev}:{node}\n{desc}\n" | Select-HgCommitMessages
-        foreach ($commit in $changeSets) {
-            if ($commit.Message -match " \#\d+") {
-                Write-Warning "Changeset with error: $($commit.Message)"
-                throw "All '#<number>' references should be prefixed by a repository name. ChangeSet Id=$($commit.LocalId)"
+function Invoke-GitCommand {
+    [CmdletBinding()]
+    param(
+        [parameter(Mandatory = $true, position = 0)]
+        [string[]]$Arguments,
+        [parameter(Mandatory = $false)]
+        [string]$Cwd,
+        [parameter(Mandatory = $false)]
+        [string]$ErrorDetailsMessage
+    )
+    process {
+
+        $env:LC_LOCAL = "C.UTF-8"
+        if ($null -eq $IsWindows -or $IsWindows) {
+            $curEncoding = [Console]::OutputEncoding
+            $encodingName = $curEncoding.WebName
+            if ($encodingName -ne "utf-8") {
+                chcp 65001 | Out-Null
+            }
+        }
+
+        try {
+            Push-Location $repoRoot
+
+            $allOutput = git $Arguments 2>&1
+            $stderr = $allOutput | ? { $_ -is [System.Management.Automation.ErrorRecord] }
+            $stdout = $allOutput | ? { $_ -isnot [System.Management.Automation.ErrorRecord] }
+
+            $stdout
+
+            if ($LASTEXITCODE -ne 0) {
+                $errorMessage = $stderr | Out-String
+                $exception = New-Object System.Management.Automation.RemoteException $errorMessage
+                $errorID = 'NativeCommandError'
+                $category = [Management.Automation.ErrorCategory]::ResourceUnavailable
+                $target = "git"
+                $errorRecord = New-Object Management.Automation.ErrorRecord $exception, $errorID, $category, $target
+                if ($ErrorDetailsMessage) {
+                    $errorRecord.ErrorDetails = New-Object Management.Automation.ErrorDetails $ErrorDetailsMessage
+                }
+                $PSCmdlet.ThrowTerminatingError($errorRecord)
+            }
+        }
+        finally {
+            Pop-Location | Out-Null
+        }
+    }
+}
+
+function Select-GitCommitMessages {
+    $curChangeset = @{}
+    $input | % {
+        if ($_[0] -eq [char]0) {
+            if ($curChangeset.Keys.Count -ne 0) {
+                $curChangeset
+                $curChangeset = @{}
+            }
+            $curChangeset.Hash = $_.Substring(1, $_.Length - 1)
+        }
+        else {
+            if (!$curChangeset.ContainsKey("Message")) {
+                $curChangeset.Message = $_
+            }
+            else {
+                $curChangeset.Message += [Environment]::NewLine + $_
             }
         }
     }
+    if ($curChangeset.Keys.Count -ne 0) {
+        $curChangeset
+    }
+}
+
+try {
+
+    if (Test-Path "$repoRoot/.hg" -Type Container) {
+        $changeSets = Invoke-HgCommand -Arguments "log", "--template", "\0{rev}:{node}\n{desc}\n" -Cwd $repoRoot | Select-HgCommitMessages
+    }
     elseif (Test-Path "$repoRoot/.git" -Type Container) {
-        Write-Host "Git!"
+        $changeSets = Invoke-GitCommand -Arguments "log", "--pretty=format:%x00%H%n%B" -Cwd $repoRoot | Select-GitCommitMessages
     }
     else {
         throw "Unknown repository type."
+    }
+
+    foreach ($commit in $changeSets) {
+        if ($commit.Message -match " \#\d+") {
+            Write-Warning "Changeset with error: $($commit.Message)"
+            throw "All '#<number>' references should be prefixed by a repository name. ChangeSet Hash=$($commit.Hash)"
+        }
     }
 }
 catch {
--- scripts/ClrSeed/ClrSeed.psd1
+++ scripts/ClrSeed/ClrSeed.psd1
@@ -34,7 +34,6 @@
     AliasesToExport   = @()
 
     RequiredModules   = @(
-        @{ModuleName = "posh-git"; ModuleVersion = "1.0.0-beta3"}
     )
     # Private data to pass to the module specified in RootModule/ModuleToProcess.
     # This may also contain a PSData hashtable with additional module metadata used by PowerShell.
